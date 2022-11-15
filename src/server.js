adc2c6dcc3e0d0e2ca44345c675f94d5ea17b9b0
hushkmj <hushkmj@naver.com>
2022-11-15 20:47:23 +0900

    session import

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 
 import express from "express"
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import storyRouter from "./routers/storyRouter";
@@ -16,6 +17,12 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 //form의 value들을 이해할 수 있도록 js형식으로 변형시켜주는 express middleWare
 app.use(express.urlencoded({ extended: true }));
+//npm express-session
+app.use(session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true
+}))
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/stories", storyRouter);
