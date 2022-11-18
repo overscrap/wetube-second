a9d9859b668c359ec2ff99bcabc03dd1502997e2
MOONJONG KIM <hushkmj@naver.com>
2022-11-18 19:02:31 +0900

    Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,30 @@
-console.log("video player!");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handleVideoPlay = (e) => {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+const handleBtnPause = () => {
+    playBtn.innerText = "Play";
+}
+const handleBtnPlay = () => {
+    playBtn.innerText = "Pause";
+}
+const handleMute = (e) => {
+
+}
+
+playBtn.addEventListener("click", handleVideoPlay);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handleBtnPause)
+video.addEventListener("play", handleBtnPlay)
+// time.addEventListener("click",)
+// volume.addEventListener("click",)
