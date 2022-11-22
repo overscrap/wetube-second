6dd5bdee8c6fccf8c27a7b22b0937bed946ca3df
hushkmj <hushkmj@naver.com>
2022-11-22 09:20:46 +0900

    Video setTime and format

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,7 +1,8 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
+const currenttime = document.getElementById("currenttime");
+const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("volume");
 
 let volumeTemp = 0.5;
@@ -46,7 +47,20 @@ const handleBtnPlay = (e) => {
     playBtn.innerText = video.paused ? "Pause" : "Play";
 }
 
+const formatTime = (second) => {
+    return new Date(second * 1000).toISOString().substring(11,19);
+}
+const handleTotalTime = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+}
+
+const handleCurrentTime = () => {
+    currenttime.innerText = formatTime(Math.ceil(video.currentTime));
+}
+
 playBtn.addEventListener("click", handlePlay);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("click", handleBtnPlay);
+video.addEventListener("loadedmetadata", handleTotalTime);
+video.addEventListener("timeupdate", handleCurrentTime);
